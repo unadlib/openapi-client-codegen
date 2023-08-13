6c5eaab9cc9f2019aae86add87f3b9223e4a4ac7
unadlib <unadlib@gmail.com>
2023-08-14 02:03:31 +0800

    feat(client): implement base client creator with proxy

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,23 @@
 import { API } from "./type";
 
+function createClientWithProxy(base = ".") {
+  return new Proxy(() => {}, {
+    get: (target, prop) => {
+      if (prop === "get") {
+        return () => {
+          console.log(base);
+        };
+      }
+      return createClientWithProxy(`${base}/${String(prop)}`);
+    },
+
+    apply: (target, thisArg, args) => {
+      const argString = args.length ? `/${args.join("~")}` : "/~";
+      return createClientWithProxy(`${base}${argString}`);
+    },
+  });
+}
+
 export const createClient = <T extends {}>(options?: {}): API<T> => {
-    // TODO: implement with proxy
-  return null as any;
+  return createClientWithProxy();
 };
